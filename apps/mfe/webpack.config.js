5f49afab51526809227bf72a24e7d9839eb5e407
Manxeguin <manxeguin1987@gmail.com>
2023-01-04 10:59:40 +0000

    first commit

---
+++ apps/mfe/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+const isProduction = process.env.NODE_ENV === "production";
+const ReactServerWebpackPlugin = require("react-server-dom-webpack/plugin");
+
+const config = {
+    mode: isProduction ? "production" : "development",
+    devtool: isProduction ? "source-map" : "cheap-module-source-map",
+    entry: [path.resolve(__dirname, "./dist/index.client.js")],
+    output: {
+        path: path.resolve(__dirname, "./build"),
+        filename: "main.[contenthash].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve(__dirname, "./public/index.html"),
+        }),
+        new ReactServerWebpackPlugin({ isServer: false }),
+    ],
+};
+
+module.exports = config;
